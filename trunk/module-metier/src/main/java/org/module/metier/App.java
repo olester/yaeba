dd81b112dcdd12d76fed6fb29d4f671c7b41a2cf
jgautier@excilys.com <jgautier@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-04-26 09:12:22 +0000

    premiere separation du pom

---
+++ trunk/module-metier/src/main/java/org/module/metier/App.java
@@ -0,0 +1,13 @@
+package org.module.metier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
