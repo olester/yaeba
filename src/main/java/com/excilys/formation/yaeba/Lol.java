b262736d081dbd46261a3f0e3b59c0e98e1937b7
jgautier@excilys.com <jgautier@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-04-26 08:46:19 +0000

    numero de versions centralisés dans le pom. Et des classes de tests pour les tests d'integration (ca passe pas...)

---
+++ src/main/java/com/excilys/formation/yaeba/Lol.java
@@ -0,0 +1,7 @@
+package com.excilys.formation.yaeba;
+
+public class Lol {
+	public String getLol() {
+		return "lol";
+	}
+}
---
+++ src/test/java/com/excilys/formation/yaeba/LolTest.java
@@ -0,0 +1,27 @@
+package com.excilys.formation.yaeba;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LolTest {
+
+	Lol lol;
+
+	@Before
+	public void setUp() throws Exception {
+		lol = new Lol();
+	}
+
+	@Test
+	public void testBadPassword() throws Exception {
+		assertTrue(lol.getLol().equals("lol"));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
