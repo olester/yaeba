32fe747da6863b50764819e7505038847cd8fa0a
olester@excilys.com <olester@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-04-28 12:59:26 +0000

    Commit du projet complet

---
+++ trunk/yaeba/yaeba-service-impl/src/main/java/com/excilys/formation/yaeba/serviceImpl/App.java
@@ -0,0 +1,13 @@
+package com.excilys.formation.yaeba.serviceImpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
