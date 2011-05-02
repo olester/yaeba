ed54baecf20e89657c11449940d0327f756c5494
hcordier@excilys.com <hcordier@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-02 17:01:43 +0000

    creation du controller du dispatcher et de web.xml

---
+++ yaeba/yaeba-webapp/src/main/java/com/excilys/formation/yaeba/webapp/UtilisateurController.java
@@ -0,0 +1,21 @@
+package com.excilys.formation.yaeba.webapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.excilys.formation.yaeba.model.Utilisateur;
+import com.excilys.formation.yaeba.service.api.UtilisateurService;
+
+@Controller
+public class UtilisateurController {
+	
+	@Autowired
+	private UtilisateurService utilisateurService;
+	
+	@RequestMapping(value="/save/utilisateur.html")
+	public void save(Utilisateur u){
+		utilisateurService.save(u);
+	}
+
+}
