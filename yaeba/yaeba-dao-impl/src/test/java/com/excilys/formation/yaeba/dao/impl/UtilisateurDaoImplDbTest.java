e627ccd3a70d88a0fd596d6cc33d84da2b07dd71
strinhkhanh@excilys.com <strinhkhanh@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-04 13:03:04 +0000

    suite du revert sur clean_insert et delete_all

--- yaeba/yaeba-dao-impl/src/test/java/com/excilys/formation/yaeba/dao/impl/UtilisateurDaoImplDbTest.java
+++ yaeba/yaeba-dao-impl/src/test/java/com/excilys/formation/yaeba/dao/impl/UtilisateurDaoImplDbTest.java
@@ -22,7 +22,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:/test/*/context/applicationContext.xml" })
+@ContextConfiguration(locations = { "classpath:/context/applicationContext.xml" })
 public class UtilisateurDaoImplDbTest {
 
 	@Autowired
