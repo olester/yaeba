d3ea96bcc6a2c5188e298177c822ac70ce8118e1
olester@excilys.com <olester@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-04 11:51:06 +0000

    dbunit test, avec ajout des dependances et d'un dataset pour les test

--- yaeba/yaeba-dao-impl/src/test/java/com/excilys/formation/yaeba/dao/impl/UtilisateurDaoImplDbTest.java
+++ yaeba/yaeba-dao-impl/src/test/java/com/excilys/formation/yaeba/dao/impl/UtilisateurDaoImplDbTest.java
@@ -1,9 +1,12 @@
 package com.excilys.formation.yaeba.dao.impl;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 
 import javax.sql.DataSource;
 
+import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
@@ -13,16 +16,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-context.xml" })
+@ContextConfiguration(locations = { "classpath:/context/applicationContext.xml" })
 public class UtilisateurDaoImplDbTest {
 
+	@Autowired
 	private DataSource dataSource;
 
-	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
@@ -30,22 +34,29 @@ public class UtilisateurDaoImplDbTest {
 	@Before
 	public void init() throws Exception {
 		// Avant le test on insère le dataSet
-		DatabaseOperation.CLEAN_INSERT.execute((IDatabaseConnection) dataSource.getConnection(), getDataSet());
+		java.sql.Connection con = DataSourceUtils.getConnection(dataSource);
+		IDatabaseConnection dbUnitCon = new DatabaseConnection(con, "yaeba");
+
+		DatabaseOperation.CLEAN_INSERT.execute(dbUnitCon, getDataSet());
 	}
 
 	@After
 	public void after() throws Exception {
-		// Après le test on supprime le dataSet
-		DatabaseOperation.DELETE_ALL.execute((IDatabaseConnection) dataSource.getConnection(), getDataSet());
+		java.sql.Connection con = DataSourceUtils.getConnection(dataSource);
+		IDatabaseConnection dbUnitCon = new DatabaseConnection(con, "yaeba");
+
+		// DatabaseOperation.DELETE_ALL.execute(dbUnitCon, getDataSet());
 	}
 
 	private IDataSet getDataSet() throws Exception {
-		return new FlatXmlDataSet(new File("src/test/resources/dataset.xml"));
+		FlatXmlDataSet f = new FlatXmlDataSet(new File("src/main/resources/dataset.xml"));
+		return f;
 	}
 
 	@Test
 	public void testSomethingWithDao() {
 		// myDaoUnderTest.doSomething();
+		assertTrue(true);
 
 	}
 }
