d0e4b12ff5657df9de8f6b6882e11b5a8092a1b0
strinhkhanh@excilys.com <strinhkhanh@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-06 14:23:09 +0000

    modèle mis à jour : méthodes addCompte, removeCompte

--- trunk/yaeba/yaeba-model/src/main/java/com/excilys/formation/yaeba/model/Compte.java
+++ trunk/yaeba/yaeba-model/src/main/java/com/excilys/formation/yaeba/model/Compte.java
@@ -94,4 +94,24 @@ public class Compte {
 		this.soldeCourant = soldeCourant;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numeroCompte == null) ? 0 : numeroCompte.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Compte other = (Compte) obj;
+		if (numeroCompte == null) {
+			if (other.numeroCompte != null) return false;
+		} else if (!numeroCompte.equals(other.numeroCompte)) return false;
+		return true;
+	}
+
 }
