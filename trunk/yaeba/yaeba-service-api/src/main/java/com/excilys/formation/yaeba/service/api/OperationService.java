6c814860c7602a7fb682c01c50446839e9ccea1d
strinhkhanh@excilys.com <strinhkhanh@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-06 14:26:53 +0000

    suppresion de la méthode "getOperationByLibelle" dans le Service

--- trunk/yaeba/yaeba-service-api/src/main/java/com/excilys/formation/yaeba/service/api/OperationService.java
+++ trunk/yaeba/yaeba-service-api/src/main/java/com/excilys/formation/yaeba/service/api/OperationService.java
@@ -1,14 +1,10 @@
 package com.excilys.formation.yaeba.service.api;
 
-import java.util.List;
-
 import com.excilys.formation.yaeba.model.Operation;
 
 public interface OperationService {
 
 	public Operation getOperationById(String id);
-
-	public List<Operation> getOperationByLibelle(String libelle);
 
 	public void update(Operation o);
 
