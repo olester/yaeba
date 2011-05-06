55e4de0956fa6c9095cc4c116d1ba54b0a7eaea7
jgautier@excilys.com <jgautier@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-06 12:48:40 +0000

    integration test selenium un peu moins basique.

--- yaeba/yaeba-webapp/src/test/java/com/excilys/formation/yaeba/selenium/LoginTest.java
+++ yaeba/yaeba-webapp/src/test/java/com/excilys/formation/yaeba/selenium/LoginTest.java
@@ -21,76 +21,78 @@ public class LoginTest {
 
 	@Test
 	public void testTest() throws Exception {
-		selenium.open("/yaeba/");
-		selenium.click("//div[@id='footer-content']/span/a[2]/img");
+		selenium.open("/yaeba-webapp/?lang=fr");
+		// assertEquals("Authentication", selenium.getTitle());
+		//
+		// selenium.click("//div[@id='footer-content']/span/a[2]/img");
 		assertEquals("Authentification", selenium.getTitle());
-		
-		 selenium.type("j_username", "user");
-		 selenium.type("j_password", "user");
-		 selenium.click("//input[@value='Valider']");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Comptes", selenium.getTitle());
-		
-		 selenium.click("link=Virements");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Virements", selenium.getTitle());
-		
-		 selenium.click("link=Accueil");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Bienvenue", selenium.getTitle());
-		
-		 selenium.click("link=Comptes");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Comptes", selenium.getTitle());
-		
-		 selenium.click("link=Yaeba");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Bienvenue", selenium.getTitle());
-		
-		 selenium.click("link=Déconnexion");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Authentification", selenium.getTitle());
-		
-		 selenium.type("j_username", "admin");
-		 selenium.type("j_password", "admin");
-		 selenium.click("//input[@value='Valider']");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Administration", selenium.getTitle());
-		
-		 selenium.click("link=Virements");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Virements", selenium.getTitle());
-		
-		 selenium.click("link=Admin");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Administration", selenium.getTitle());
-		
-		 selenium.click("link=Comptes");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Comptes", selenium.getTitle());
-		
-		 selenium.click("link=Accueil");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Bienvenue", selenium.getTitle());
-		
-		 selenium.click("link=Comptes");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Comptes", selenium.getTitle());
-		
-		 selenium.click("link=Yaeba");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Bienvenue", selenium.getTitle());
-		
-//		 selenium.open("/yaeba-webapp/unepageinexistante");
-//		 assertEquals("Erreur", selenium.getTitle());
-		
-//		 selenium.click("link=Revenir a la page d'accueil");
-//		 selenium.waitForPageToLoad("30000");
-//		 assertEquals("Bienvenue", selenium.getTitle());
-//		
-		 selenium.click("link=Déconnexion");
-		 selenium.waitForPageToLoad("30000");
-		 assertEquals("Authentification", selenium.getTitle());
+
+		selenium.type("j_username", "user");
+		selenium.type("j_password", "user");
+		selenium.click("//input[@value='Valider']");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Comptes", selenium.getTitle());
+
+		selenium.click("link=Virements");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Virements", selenium.getTitle());
+
+		selenium.click("link=Accueil");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Bienvenue", selenium.getTitle());
+
+		selenium.click("link=Comptes");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Comptes", selenium.getTitle());
+
+		selenium.click("link=Yaeba");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Bienvenue", selenium.getTitle());
+
+		selenium.click("link=Déconnexion");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Authentification", selenium.getTitle());
+
+		selenium.type("j_username", "admin");
+		selenium.type("j_password", "admin");
+		selenium.click("//input[@value='Valider']");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Administration", selenium.getTitle());
+
+		selenium.click("link=Virements");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Virements", selenium.getTitle());
+
+		selenium.click("link=Admin");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Administration", selenium.getTitle());
+
+		selenium.click("link=Comptes");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Comptes", selenium.getTitle());
+
+		selenium.click("link=Accueil");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Bienvenue", selenium.getTitle());
+
+		selenium.click("link=Comptes");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Comptes", selenium.getTitle());
+
+		selenium.click("link=Yaeba");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Bienvenue", selenium.getTitle());
+
+		// selenium.open("/yaeba-webapp/unepageinexistante");
+		// assertEquals("Erreur", selenium.getTitle());
+
+		// selenium.click("link=Revenir a la page d'accueil");
+		// selenium.waitForPageToLoad("30000");
+		// assertEquals("Bienvenue", selenium.getTitle());
+		//
+		selenium.click("link=Déconnexion");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Authentification", selenium.getTitle());
 
 	}
 
