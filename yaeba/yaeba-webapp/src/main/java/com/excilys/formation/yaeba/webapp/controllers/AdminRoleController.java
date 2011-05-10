f391167987b68ed82182f362c7d240a4d074c362
strinhkhanh@excilys.com <strinhkhanh@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-10 14:26:13 +0000

    affichage du nom/prénom pour la page admin

--- yaeba/yaeba-webapp/src/main/java/com/excilys/formation/yaeba/webapp/controllers/AdminRoleController.java
+++ yaeba/yaeba-webapp/src/main/java/com/excilys/formation/yaeba/webapp/controllers/AdminRoleController.java
@@ -3,9 +3,13 @@ package com.excilys.formation.yaeba.webapp.controllers;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.excilys.formation.yaeba.model.Utilisateur;
+import com.excilys.formation.yaeba.webapp.CustomUser;
 
 @Controller
 @RequestMapping("/admin")
@@ -13,10 +17,14 @@ public class AdminRoleController {
 
 	@RequestMapping("/admin.html")
 	public String redirectComptes(ModelMap model, Locale locale) {
-		ResourceBundle bundle = ResourceBundle.getBundle("messages_"+locale.getLanguage());
+		ResourceBundle bundle = ResourceBundle.getBundle("messages_" + locale.getLanguage());
 		model.put("title", bundle.getString("admin.title"));
 		model.put("bouton", "bouton_comptes");
 		model.put("bouton", "bouton_admin");
+
+		Utilisateur u = ((CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUtilisateur();
+		model.put("utilisateur", u);
+
 		return "admin";
 	}
 
