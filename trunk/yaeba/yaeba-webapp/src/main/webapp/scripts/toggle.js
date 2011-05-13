dbbcb73e28f94ec8f831ec19408ba2a8a6fcf73a
strinhkhanh@excilys.com <strinhkhanh@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-13 16:40:35 +0000

    jsp : flèche qui rotate au toggle sur la liste des opérations CB

--- trunk/yaeba/yaeba-webapp/src/main/webapp/scripts/toggle.js
+++ trunk/yaeba/yaeba-webapp/src/main/webapp/scripts/toggle.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
+	$("#action2").click(function() {
+		$("#action").click();
+	});
 	$("#action").click(function() {
-			$("#deroulant").slideToggle();
+		if ($("#deroulant").css("display") == "block") {
+			$("#arrow").rotate(0);
+			$("#deroulant").slideUp();
+		}
+		else {
+			$("#arrow").rotate(90);
+			$("#deroulant").slideDown();
+		}
 	});
 	$("#close").click(function() {
 		if ($("#deroulant").css("display") == "block") {
 			$("#deroulant").slideUp();
+			$("#arrow").rotate(0);
 		}
 	});
 });
