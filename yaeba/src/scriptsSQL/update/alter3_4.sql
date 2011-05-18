136510fce341cee4b13a461333f89d565b68abee
strinhkhanh@excilys.com <strinhkhanh@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-18 16:10:54 +0000

    oups (erreur de frappe dans le script tout juste commité)

--- yaeba/src/scriptsSQL/update/alter3_4.sql
+++ yaeba/src/scriptsSQL/update/alter3_4.sql
@@ -1,2 +1,2 @@
-ALTER TABLE  `compte` CHANGE  `soldecourant`  `soldecourant` DECIMAL( 19, 3 ) NOT NULL;
+ALTER TABLE  `compte` CHANGE  `soldecourant`  `soldecourant` DECIMAL( 19, 2 ) NOT NULL;
 ALTER TABLE  `operation` CHANGE  `montant`  `montant` DECIMAL( 19, 2 ) NOT NULL;
