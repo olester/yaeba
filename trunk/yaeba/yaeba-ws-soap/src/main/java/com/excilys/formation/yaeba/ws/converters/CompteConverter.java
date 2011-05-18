7300a061885dd743b878da0f6ffae5c9fb42fe43
jgautier@excilys.com <jgautier@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-18 15:57:31 +0000

    Changement des converters après la remarque de Stéphane !

--- trunk/yaeba/yaeba-ws-soap/src/main/java/com/excilys/formation/yaeba/ws/converters/CompteConverter.java
+++ trunk/yaeba/yaeba-ws-soap/src/main/java/com/excilys/formation/yaeba/ws/converters/CompteConverter.java
@@ -1,18 +1,21 @@
 package com.excilys.formation.yaeba.ws.converters;
 
+import org.springframework.core.convert.converter.Converter;
+
 import com.excilys.formation.yaeba.model.Compte;
 import com.excilys.formation.yaeba.ws.InfoCompte;
 
-public class CompteConverter {
+public class CompteConverter implements Converter<Compte, InfoCompte> {
 
-	public static InfoCompte compteToInfoCompte(Compte c) {
+	@Override
+	public InfoCompte convert(Compte c) {
 		InfoCompte ic = new InfoCompte();
 		ic.setId(c.getId());
 		ic.setLibelle(c.getLibelle());
 		ic.setNumero(c.getNumeroCompte());
 		ic.setSolde(c.getSoldeCourant());
 		ic.setPrevisional(c.getEncoursCarte());
-		ic.setUtilisateur(UtilisateurConverter.utilisateurToInfoUtilisateur(c.getUtilisateur()));
+		ic.setUtilisateur(new UtilisateurConverter().convert(c.getUtilisateur()));
 		return ic;
 	}
 }
