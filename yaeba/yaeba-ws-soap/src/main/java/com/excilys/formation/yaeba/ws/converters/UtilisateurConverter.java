15034c61be12ce2d8b620b06d20c3bd23075f7a7
jgautier@excilys.com <jgautier@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-19 08:53:10 +0000

    correction d'ereur sur les ws

--- yaeba/yaeba-ws-soap/src/main/java/com/excilys/formation/yaeba/ws/converters/UtilisateurConverter.java
+++ yaeba/yaeba-ws-soap/src/main/java/com/excilys/formation/yaeba/ws/converters/UtilisateurConverter.java
@@ -1,10 +1,12 @@
 package com.excilys.formation.yaeba.ws.converters;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 import com.excilys.formation.yaeba.model.Utilisateur;
 import com.excilys.formation.yaeba.ws.InfoUtilisateur;
 
+@Component
 public class UtilisateurConverter implements Converter<Utilisateur, InfoUtilisateur> {
 
 	@Override
