b3c8fb1c7189db9d4c09daa7d77338ce696a9e3f
flemarchand@excilys.com <flemarchand@excilys.com@ccb4cfeb-8757-0f03-6331-c1c4f79e50c4>
2011-05-19 09:26:06 +0000

    Ajout des injection de bean , compteconverter services SOAP et REST

--- trunk/yaeba/yaeba-ws-soap/src/main/java/com/excilys/formation/yaeba/ws/converters/CompteConverter.java
+++ trunk/yaeba/yaeba-ws-soap/src/main/java/com/excilys/formation/yaeba/ws/converters/CompteConverter.java
@@ -1,5 +1,6 @@
 package com.excilys.formation.yaeba.ws.converters;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,9 @@ import com.excilys.formation.yaeba.ws.InfoCompte;
 @Component
 public class CompteConverter implements Converter<Compte, InfoCompte> {
 
+	@Autowired
+	private UtilisateurConverter utilisateurConverter;
+
 	@Override
 	public InfoCompte convert(Compte c) {
 		InfoCompte ic = new InfoCompte();
@@ -17,7 +21,7 @@ public class CompteConverter implements Converter<Compte, InfoCompte> {
 		ic.setNumero(c.getNumeroCompte());
 		ic.setSolde(c.getSoldeCourant());
 		ic.setPrevisional(c.getEncoursCarte());
-		ic.setUtilisateur(new UtilisateurConverter().convert(c.getUtilisateur()));
+		ic.setUtilisateur(utilisateurConverter.convert(c.getUtilisateur()));
 		return ic;
 	}
 }
